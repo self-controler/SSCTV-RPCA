35fe1331596b7eb37faa1fd861df459c4235f7d8
none <143919887+self-controler@users.noreply.github.com>
2024-06-03 21:37:16 +0800

    Update Demo_of_HSI_denoising.m

--- Denoising/Demo_of_HSI_denoising.m
+++ Denoising/Demo_of_HSI_denoising.m
@@ -48,4 +48,62 @@ t(it)=toc;
 rpca_out = reshape(A_hat,[M,N,p]);
 [mpsnr(it),mssim(it),ergas(it)]=msqia(clean_data, rpca_out);
 
+%% VBRPCA
+it=5;
+fprintf('========   VBRPCA  ========\n')
+tic;
+[X,~] = VBRPCA(D);
+t(it)=toc;
+vbrpca_out = reshape(X,[M,N,p]);
+[mpsnr(it),mssim(it),ergas(it)]=msqia(clean_data, vbrpca_out);
 
+%% LRTDTV
+it=6;
+fprintf('========   LRTDTV  ========\n')
+tic;
+[A_hat,~]=LRTDTV(noise_data,0.02,1,[100,100,20]);
+t(it)=toc;
+LRTDTV_out = reshape(A_hat,[M,N,p]);
+[mpsnr(it),mssim(it),ergas(it)]=msqia(clean_data, LRTDTV_out);
+
+%% LRTV
+it =7;
+fprintf('======== LRTV  ========\n')
+tic;
+[lrtv_out, out_value] = LRTV(noise_data,0.1,1,20);
+t(it)=toc;
+[mpsnr(it),mssim(it),ergas(it)]=msqia(clean_data, lrtv_out);
+
+%% PRMF
+it=8;
+fprintf('========   PRMF  ========\n')
+tic;
+[U,V]=RPMF(D,5,1,1,1e-6);
+t(it)=toc;
+prmf_out = reshape(U*V,[M,N,p]);
+[mpsnr(it),mssim(it),ergas(it)]=msqia(clean_data, prmf_out);
+
+%% LRMR
+it=9;
+fprintf('========   LRMR  ========\n')
+tic;
+[A_hat,~]=MSILRMR(noise_data,0.01,10);
+t(it)=toc;
+LRMR_out = reshape(A_hat,[M,N,p]);
+[mpsnr(it),mssim(it),ergas(it)]=msqia(clean_data, LRMR_out);
+
+%% SSTV
+it =10;
+fprintf('======== SSTV  ========\n')
+tic;
+[sstv_out, out_value] = sstv(noise_data,0.3);
+t(it)=toc;
+[mpsnr(it),mssim(it),ergas(it)]=msqia(clean_data, sstv_out);
+
+%% LRSSTV
+it =11;
+fprintf('======== LRSSTV  ========\n')
+tic;
+[lrsstv_out, out_value] = lrsstv(noise_data,0.2,2);
+t(it)=toc;
+[mpsnr(it),mssim(it),ergas(it)]=msqia(clean_data, lrsstv_out);
